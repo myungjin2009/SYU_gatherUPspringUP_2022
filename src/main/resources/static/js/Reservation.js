8f1143c0556557fc58858e67c4cb9f8fb66beef1
Kim Myeongjin <myungjin2009@naver.com>
2022-09-26 23:59:57 +0900

    게시글 삭제 기능 추가중, 임시 커밋

--- src/main/resources/static/js/Reservation.js
+++ src/main/resources/static/js/Reservation.js
@@ -1,5 +1,15 @@
 
 
 function reserveRoom() {
-    console.log("버튼 눌림");
+        $.ajax({
+            type: 'post',
+            url: `/board/Delete`,
+            async: false,
+            success: function (result){
+                alert(result);
+            },error: function (error){
+                alert(error.responseText);
+            }
+        })
+
 }
